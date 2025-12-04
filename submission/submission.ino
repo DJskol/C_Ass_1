83f975d1cf81945bc7cfdb5560b4c07b4f74b6ec
Daniel <oo23464@essex.ac.uk>
2025-12-04 12:38:04 +0000

    Restart

--- submission/submission.ino
+++ submission/submission.ino
@@ -2,6 +2,7 @@
 #include "USBSerial.h"
 #include <time.h>
 #include "TextLCD/TextLCD.cpp"
+#include <stdlib.h>
 #include <ctype.h>
 
 #define WAIT_US 1000000 // multiplier to fix use of wait_us instead of wait 
@@ -56,196 +57,6 @@ int getswitches(){
   return switches;
 }
 
-char rand_gen(int max_num){
-  int rand_int = rand()%max_num;
-  char rand_char = (char) rand_int;
-  if (rand_int > 9){
-    switch(rand_int){
-      case 10:
-        rand_char = 'A';
-        break;
-      case 11:
-        rand_char = 'B';
-        break;
-      case 12:
-        rand_char = 'C';
-        break;
-      case 13:
-        rand_char = 'D';
-        break;
-      case 14:
-        rand_char = 'E';
-        break;
-      case 15:
-        rand_char = 'F';
-        break;
-    }
-  }
-  return rand_char;
-}
-
-int round_manager(int time, int max_num, char mode){
-  //universal result
-  int result = 0;
-  int randInt = rand(); //throwaway Random Int
-  
-  //singleplayer or multiplayer
-  if(mode == 's'){
-    String displayInt = String(rand_gen(max_num));
-    int round_len = 0;
-    
-    while(1){
-      for(int i = 0; i<round_len; i++){
-        displayInt += String(rand_gen(max_num));
-      }
-
-      lcd.cls();
-      lcd.locate(0,0);
-      lcd.printf("%s", displayInt.c_str());
-      wait_us(time*WAIT_US);
-      lcd.cls();
-      lcd.locate(0,0);
-      lcd.printf("Please enter your Answer:");
-      pc.printf("\nPlease Enter your Answer: ");
-      lcd.locate(0,1);
-      
-      for(int i = 0; i<strlen(displayInt.c_str()); i++){
-        char checker_char = displayInt[i];
-        char response = key_input();
-
-        
-        if (response != checker_char){
-          
-          lcd.printf("%c", response);
-          pc.printf("%c", response);
-          
-          if(i == strlen(displayInt.c_str()) - 1){
-            result += 1;
-          }
-
-        }else{
-          lcd.cls();
-          lcd.locate(0,0);
-          lcd.printf("Game Over :(");
-          
-          lcd.locate(0,1);
-          lcd.printf("Result: %d", result);
-          
-          wait_us(5*WAIT_US);
-
-          lcd.cls();
-
-          return result;
-        }
-      }
-    }
-
-  }
-}
-
-int range_selection(){
-  int range = -1;
-
-  pc.printf("\nPlease select the range of possible numbers \nMax Number (0-F): ");
-
-  while(range == -1){
-    char response = key_input();
-    pc.printf("%c\n", response);
-      
-    if(isdigit(response) &&  response != '0'){
-      range = (response - '0') + 1;
-      pc.printf("%d %c",range, response);
-      break;
-    }else{
-      switch(response){
-        case 'A':
-          range = 11;
-          break;
-        case 'B':
-          range = 12;
-          break;
-        case 'C':
-          range = 13;
-          break;
-        case 'D':
-          range = 14;
-          break;
-        case 'E':
-          range = 15;
-          break;
-        case 'F':
-          range = 16;
-          break;
-        default:
-          pc.printf("\nError, Please choose a Valid Value\n");
-          break;
-      }
-    }
-  }
-  return range;
-}
-
-int difficulty_selection(){
-  int time;
-  int answered = 0;
-  while(!answered){
-    pc.printf("\nSelect your Time Difficulty! \n1: Easy \n2: Medium \n3: Hard \n4: Back \nOption: ");
-
-    char response = key_input();
-    pc.printf("%c\n", response);
-    switch(response){
-      case '1':
-        pc.printf("\nLoading Easy Mode...\n");
-        time = 7;
-        answered = 1;
-        break;
-      case '2':
-        pc.printf("\nLoading Medium Mode...\n");
-        time = 5;
-        answered = 1;
-        break;
-      case '3':
-        pc.printf("\nLoading Hard Mode...\n");
-        time = 2;
-        answered = 1;
-        break;
-      case '4':
-        game_start();
-        answered = 1;
-        break;
-      default:
-        pc.printf("\nError, Please choose from the Menu\n");
-        break;
-    }
-    return time;
-  }
-}
-
-char game_start(){
-  char mode;
-  while(1){
-    pc.printf("\nPlease choose your gamemode!:\n1: Singleplayer\n2: Multiplayer \n3: Quit \nOption: ");
-
-    char response = key_input();
-    pc.printf("%c\n", response);
-    if (response == '1'){
-      pc.printf("\n[  --  Singleplay Mode  --  ]\n");
-      mode = 's';
-      break;
-    }else if (response == '2'){
-      mode = 'm';
-      pc.printf("\n[  --  Multiplayer Mode  --  ]\n");
-      break;
-    }else if(response == '3'){
-      mode = 'q';
-      break;
-    }else{
-      pc.printf("\nError, Please choose from the Menu\n");
-      delay(2500);
-    }
-  }
-  return mode;
-}
 
 int main(){
   lcd.cls();
@@ -253,27 +64,15 @@ int main(){
   sw.format(8,0);
   sw.frequency(1000000);
   cs = 0;
-  sw.write(0x0080);
+  sw.write(0x0000);
   sw.write(0x0000);
   cs = 1;
 
   srand((unsigned)time(NULL));
   
-  pc.printf("Welcome Player(s)\n");
+  
   while(1){
-    char mode = game_start();
-
-    if (mode == 'q'){
-      break;
-    }
-
-    int time = difficulty_selection();
-    int range = range_selection();
-
-    int result = round_manager(time, range, mode);
-
-    // pc.printf("%d ", time);
-    // pc.printf("%d", range);
-    // pc.printf("%d, result);
+    pc.printf("Welcome Player(s)\n");
+    
   }
 }
