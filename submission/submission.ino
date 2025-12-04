c83aa7b0f2306ad27e0c17616d7ee5d77b5cb357
Daniel <oo23464@essex.ac.uk>
2025-12-04 13:32:12 +0000

    solo game plan + main menu and basic pc input working

--- submission/submission.ino
+++ submission/submission.ino
@@ -2,11 +2,12 @@
 #include "USBSerial.h"
 #include <time.h>
 #include "TextLCD/TextLCD.cpp"
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 #define WAIT_US 1000000 // multiplier to fix use of wait_us instead of wait 
-
+//Default setup from labscripts
 using namespace mbed;
 USBSerial pc; // tx, rx
 SPI sw(p3, NC, p2);
@@ -22,7 +23,7 @@ char Keytable[] = { 'F', 'E', 'D', 'C',
 '1', '4', '7', 'A'
 };
 
-char getKey(){ 
+char get_Key(){ 
   int i,j; 
   char ch=' ';
   for (i = 0; i <= 3; i++) { 
@@ -39,7 +40,7 @@ char getKey(){
 char key_input(){
   char key = ' '; 
   while(1) {
-    key = getKey();
+    key = get_Key();
     if(key != ' '){
       // pc.printf("Key = %c\r\n", key);
       wait_us(0.3*WAIT_US);
@@ -48,7 +49,7 @@ char key_input(){
   }
 }
 
-int getswitches(){
+int get_switches(){
   bus_out = 4;
   int switches = bus_in;
   bus_out = 5;
@@ -56,6 +57,9 @@ int getswitches(){
 
   return switches;
 }
+
+//Custom code
+//Processing functions
 
 
 int main(){
@@ -69,10 +73,38 @@ int main(){
   cs = 1;
 
   srand((unsigned)time(NULL));
+
+  int active = 1;
   
-  
-  while(1){
-    pc.printf("Welcome Player(s)\n");
+  while(active){
+    char raw_menu_choice = '0';
+    pc.printf("\nWelcome Player(s)\n");
+    
+    pc.printf("\nPlease Choose Your Game Mode!\n");
+    pc.printf("1: Singleplayer \n2: Multiplayer \n3:Quit \nAnswer: ");
+
+    while(1){
+      raw_menu_choice = pc.getc();
+      if(raw_menu_choice != '0'){
+        break;
+      }
+    }
+
+    switch(raw_menu_choice){
+      case '1':
+        pc.printf("%d\n\n",raw_menu_choice);
+        break;
+      case '2':
+        pc.printf("%d\n\n",raw_menu_choice);
+        break;
+      case '3':
+        pc.printf("Goodbye!\n\n");
+        active = 0;
+        break;
+      default:
+        pc.printf("Wrong Option, Please Choose Approprately\n\n");
+        break;
+    }
     
   }
 }
